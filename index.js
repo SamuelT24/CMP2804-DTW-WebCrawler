a4c9cb7c42d87673be54271f1de9e10d183a3900
Samuel T <samueltracey24@yahoo.com>
2024-03-30 17:03:08 +0000

    index: add support for users to specify a URL

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer"); // Having issues? Make sure this is installed! It's not a built-in module!
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const getArticleLink = () => {
+  // getArticleLink()
+  // This is a temporary function for testing purposes where we can specify
+  // an article to test the scraper on. Note that this will be removed once
+  // we have a proper GUI up and running.
+  return new Promise(resolve => {
+    readline.question("Please enter an article to scrape: ", (pageLink) => {
+      readline.close();
+      resolve(pageLink);
+    });
+  });
+};
 
 (async () => {
-  // Launch the browser
+  // We generate our puppeteer browser here, then scrape the user specified link.
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // Navigate to the target webpage
-  await page.goto('https://www.theguardian.com/world/2024/mar/24/new-islamic-state-videos-back-claim-it-carried-out-moscow-concert-hall-attack');
+  // TODO: This works for the time being, but later we should definitely
+  // add some exception handling in case the user enters something stupid,
+  // or the specified URL is unavailable.
+  await page.goto(await getArticleLink());
 
-  // Scrape the title and article text
   const articleData = await page.evaluate(() => {
-    const articleTitle = document.querySelector('h1').innerText;
-    const articleText = Array.from(document.querySelectorAll('article p')).map(p => p.innerText).join('\n');
+    // TODO: This is a good start, but on our actual implementation we should probably
+    // format this nicer, with some consideration for the potential of subtitles.
+    const articleTitle = document.querySelector("h1").innerText;
+    const articleText = Array.from(document.querySelectorAll("article p")).map(p => p.innerText).join("\n");
 
     return {
       title: articleTitle,
@@ -19,12 +39,11 @@ const puppeteer = require('puppeteer');
     };
   });
 
-  // Define a list to hold your scraped data and append the article data to it
   let dataList = [];
   dataList.push(articleData);
 
-  console.log(dataList);
+  console.log(dataList); // For the time being, display our dataList by logging it in the console.
+  // We'll display this nicer once we have a GUI up and running!
 
-  // Close the browser
   await browser.close();
 })();
